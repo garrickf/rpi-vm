fe6d9a833918aef5acce1bfec3e5bc721b599fc5
garrickf <gfaerr@stanford.edu>
2019-01-30 18:55:07 -0800

    move on to part 3 of lab 6

--- homeworks/1-bootloader/bootloader/echo/echo.c
+++ homeworks/1-bootloader/bootloader/echo/echo.c
@@ -19,7 +19,7 @@
 // simple state machine to indicate when we've seen a special string
 // from the pi telling us to shutdown.
 static int done(unsigned char *s) {
-	static unsigned pos = 0;
+  static unsigned pos = 0;
 	const char exit_string[] = "DONE!!!\n";
 	const int n = sizeof exit_string - 1;
 
@@ -38,44 +38,44 @@ static int done(unsigned char *s) {
 
 #include <errno.h>
 
-#define note(msg...) do {                                              \
-        fprintf(stderr, "echo:"); 					\
-        fprintf(stderr, ##msg);                                         \
+#define note(msg...) do { \
+fprintf(stderr, "echo:"); \
+fprintf(stderr, ##msg);   \
 } while(0)
 
 
 // read and echo the characters from the tty until it closes (pi rebooted)
 // or we see a string indicating a clean shutdown.
 static void echo(int fd) {
-        while(1) {
-                unsigned char buf [4096];
-                int n = read (fd, buf, sizeof buf - 1);
-                if(!n) {
-                        struct stat s;
+	while(1) {
+		unsigned char buf [4096];
+		int n = read (fd, buf, sizeof buf - 1);
+		if(!n) {
+			struct stat s;
 			// change this to fstat.
-                        if(fstat(fd, &s) < 0) {
+			if(fstat(fd, &s) < 0) {
 				note("read EOF\n");
 				perror("fstat");
-                                note("pi connection closed.  cleaning up\n");
-                                exit(0);
-                        }
+				note("pi connection closed.  cleaning up\n");
+				exit(0);
+			}
 			// gonna have to override this if you want to test.
-                        usleep(1000);
-                } else if(n < 0) {
+			usleep(1000);
+		} else if(n < 0) {
 			note("ERROR: got %s res=%d\n", strerror(n),n);
-                        note("echo: pi connection closed.  cleaning up\n");
-                        exit(0);
+			note("echo: pi connection closed.  cleaning up\n");
+			exit(0);
 		} else {
-                        buf[n] = 0;
+			buf[n] = 0;
                         // XXX printf does not flush until newline!
-                        fprintf(stderr, "%s", buf);
+			fprintf(stderr, "%s", buf);
 
 			if(done(buf)) {
 				note("\necho: Saw done\n");
-                        	note("\necho: bootloader: pi exited.  cleaning up\n");
-                        	exit(0);
+				note("\necho: bootloader: pi exited.  cleaning up\n");
+				exit(0);
 			}
-                }
+		}
 	}
 }
 int main(void) {
